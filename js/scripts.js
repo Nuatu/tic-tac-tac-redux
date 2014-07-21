978e935d9601484bed683a22d2dddcee7e49202f
Nick Bernard & Nuatu Tseggai <student@epicodus.com>
2014-07-21 13:35:06 -0700

    Create a board with 9 spaces

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,50 @@
 var Player = {
   initialize: function(inputSymbol) {
     this.symbol = inputSymbol;
+  },
+  create: function(inputSymbol) {
+    var testPlayer = Object.create(Player);
+    testPlayer.initialize(inputSymbol);
+    return testPlayer;
   }
 };
 
 var Space = {
   initialize: function(x,y) {
-    this.xCoordinate= x;
-    this.yCoordinate= y;
-  }
+    this.xCoordinate = x;
+    this.yCoordinate = y;
+  },
+  create: function(x,y) {
+    var testSpace = Object.create(Space);
+    testSpace.initialize(x,y);
+    return testSpace;
+  },
+  markBy: function(symbol) {
+    this.markedBy = symbol;
+    return this.markedBy;
+  },
 };
+
+
+var Board = {
+  initialize: function() {
+    this.space1 = Space.create(1,1);
+    this.space2 = Space.create(1,2);
+    this.space3 = Space.create(1,3);
+    this.space4 = Space.create(2,1);
+    this.space5 = Space.create(2,2);
+    this.space6 = Space.create(2,3);
+    this.space7 = Space.create(3,1);
+    this.space8 = Space.create(3,2);
+    this.space9 = Space.create(3,3);
+  }
+    // var coordinates = [[1,1],[1,2],[1,3],[2,1],[2,2],[2,3],[3,1],[3,2],[3,3]];
+    // this.spaces = [];
+    // coordinates.forEach(function(coordinate) {
+    //   var newSpace = Object.create(Space);
+    //   newSpace.initialize(coordinate[0],coordinate[1]);
+    //   console.log(newSpace);
+    //   this.spaces.push(newSpace);
+    //   console.log(this.boardSpaces);
+    // });
+}
--- specs/specs.js
+++ specs/specs.js
@@ -6,6 +6,14 @@ describe("Player", function() {
       testPlayer.symbol.should.equal("X");
     });
   });
+
+  describe("create", function() {
+    it("creates a new Player object", function() {
+      var testPlayer = Object.create(Player);
+      testPlayer.initialize("X");
+      Player.isPrototypeOf(testPlayer).should.equal(true);
+    });
+  });
 });
 
 describe("Space", function() {
@@ -15,6 +23,34 @@ describe("Space", function() {
       testSpace.initialize(1, 2);
       testSpace.xCoordinate.should.equal(1);
       testSpace.yCoordinate.should.equal(2);
+    });
+  });
+  describe("create", function() {
+    it("creates a new Space object", function() {
+      var testSpace = Object.create(Space);
+      testSpace.initialize(1, 2);
+      Space.isPrototypeOf(testSpace).should.equal(true);
+    });
+  });
+  describe("markBy", function() {
+    it("lets a player mark the space", function() {
+      var testPlayer = Object.create(Player);
+      testPlayer.initialize("X");
+      var testSpace = Object.create(Space);
+      testSpace.initialize(1, 2);
+      testSpace.markBy(testPlayer);
+      testSpace.markedBy.should.equal(testPlayer);
+    });
+  });
+});
+
+describe("Board", function() {
+  describe("initialize", function() {
+    it("creates 9 spaces when it is initialized", function() {
+      var testBoard = Object.create(Board);
+      testBoard.initialize();
+      testBoard.space1.xCoordinate.should.equal(1);
+      testBoard.space9.yCoordinate.should.equal(3);
     });
   });
 });
