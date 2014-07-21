56ec165cdeacf4694ed48f87be3443e3b0f380ba
Nick Bernard & Nuatu Tseggai <student@epicodus.com>
2014-07-21 10:05:43 -0700

    Set up initial specs and prototypes for players and spaces

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var Player = {
+  initialize: function(inputSymbol) {
+    this.symbol = inputSymbol;
+  }
+};
+
+var Space = {
+  initialize: function(x,y) {
+    this.xCoordinate= x;
+    this.yCoordinate= y;
+  }
+};
---
+++ specs/specs.js
@@ -0,0 +1,22 @@
+describe("Player", function() {
+  describe("initialize", function() {
+    it("is initialized with a symbol", function() {
+      var testPlayer = Object.create(Player);
+      testPlayer.initialize("X");
+      testPlayer.symbol.should.equal("X");
+    });
+  });
+});
+
+describe("Space", function() {
+  describe("initialize", function() {
+    it("is initialized with an x and y coordinate", function() {
+      var testSpace = Object.create(Space);
+      testSpace.initialize(1, 2);
+      testSpace.xCoordinate.should.equal(1);
+      testSpace.yCoordinate.should.equal(2);
+    });
+  });
+});
+
+
